c755b50ae21ee206869bdb2b0c49e2670933448d
abeelha <dev.theodoro@gmail.com>
2025-10-20 09:37:23 -0300

    Add analytics vercel

--- observablehq.config.js
+++ observablehq.config.js
@@ -17,7 +17,13 @@ export default {
   // ],
 
   // Content to add to the head of the page, e.g. for a favicon:
-  head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
+  head: `
+    <link rel="icon" href="observable.png" type="image/png" sizes="32x32">
+    <script>
+      window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
+    </script>
+    <script defer src="/_vercel/insights/script.js"></script>
+  `,
 
   // The path to the source root.
   root: "src",
